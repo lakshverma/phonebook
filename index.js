3efd92e163b38e060466e53311abfac7bee80687
Lakshay Verma <lakshverma23@gmail.com>
2021-09-27 21:11:20 +0530

    Fix code formatting and add a custom token for logging with Morgan

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const { request, response } = require('express')
-const express = require('express')
-var morgan = require('morgan')
+const { request, response } = require("express");
+const express = require("express");
+var morgan = require("morgan");
 
-const app = express()
-app.use(morgan('tiny'))
+const app = express();
 
-app.use(express.json())
+morgan.token("bodyObject", (request) => JSON.stringify(request.body));
+
+// Logging sensitive data not a great practice, this is just for testing tokens
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :bodyObject"
+  )
+);
+
+app.use(express.json());
 
 let people = [
   { 
@@ -28,63 +36,61 @@ let people = [
     "name": "Mary Poppendieck", 
     "number": "39-23-6423122"
   }
-]
+];
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
+app.get("/api/people", (request, response) => {
+  response.json(people);
+});
 
-app.get('/api/people', (request, response) => {
-  response.json(people)
-})
-
-app.get('/info', (request, response) => {
+app.get("/info", (request, response) => {
   response.send(`<p>Phonebook has info for ${people.length} people </p>
-  <p>${new Date()}</p>`)
-})
+  <p>${new Date()}</p>`);
+});
 
-app.get('/api/people/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = people.find(person => person.id === id)
-  response.json(person)
-})
+app.get("/api/people/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = people.find((person) => person.id === id);
+  response.json(person);
+});
 
-app.post('/api/people/', (request, response) => {
-  const body = request.body
+app.post("/api/people/", (request, response) => {
+  const body = request.body;
 
   if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
-  if (people.find(person => person.name === body.name)) {
+  if (people.find((person) => person.name === body.name)) {
     return response.status(422).json({
-      error: 'name must be unique'
-    })
+      error: "name must be unique",
+    });
   }
 
   const person = {
-    id: (Math.floor(Math.random()*100000) + 1),
+    id: Math.floor(Math.random() * 100000) + 1,
     name: body.name,
-    number: body.number
-  }
+    number: body.number,
+  };
 
-  people = people.concat(person)
+  people = people.concat(person);
 
-  response.json(person)
+  response.json(person);
+});
 
-})
+app.delete("/api/people/:id", (request, response) => {
+  const id = Number(request.params.id);
+  people = people.filter((person) => person.id != id);
 
-app.delete('/api/people/:id', (request, response) => {
-  const id = Number(request.params.id)
-  people = people.filter(person => person.id != id);
+  response.status(204).end();
+});
 
-  response.status(204).end()
-})
-
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
